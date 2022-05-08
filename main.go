00e1b959fa82b3b9f4ae2e03c755c6bb4f5b4515
Anmar <eng.anmarhomod@gmail.com>
2022-05-08 18:30:44 +0300

    Setting up the server

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func handleConnection(conn net.Conn) error {
+	log.Println("I got a connection")
+	return nil
+}
+
+func startServer() error {
+	log.Println("Starting Server")
+
+	ln, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		return err
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println("Error accepting connection", err)
+			continue
+		}
+		go func() {
+			if err := handleConnection(conn); err != nil {
+				log.Println("Error handling connection", err)
+				return
+			}
+		}()
+
+	}
+}
+
+func main() {
+	err := startServer()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
